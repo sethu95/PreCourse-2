1a85394ed9be25118ba35d05522473b2da7a1790
Sethu Manickam <sethu.sxcs@gmail.com>
2025-06-25 16:32:53 -0700

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,3 +1,6 @@
+// Time Complexity: O(n log n) this is the average case complexity. If all our pivot elements are positioned in the end that is, if array is in descending order time complexity is O(n)
+// Space Complexity: O(1)
+
 class QuickSort 
 { 
     /* This function takes last element as pivot, 
@@ -7,13 +10,33 @@ class QuickSort
        pivot and all greater elements to right 
        of pivot */
     void swap(int arr[],int i,int j){
-        //Your code here   
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
     
     int partition(int arr[], int low, int high) 
     { 
-   	//Write code here for Partition and Swap 
+   	    //Write code here for Partition and Swap 
+        int i = low-1;
+        int pivot = arr[high];
+        
+        
+        // moving all smaller elements to the left of the pivot
+        for (int j=low; j<high; j++) {
+            if (arr[j] < pivot) {
+                i++; 
+                swap(arr, i, j);
+            }
+        }
+        
+        // moving the pivot element after all smaller elements are moved. i+1 is the correct position for arr[high]
+        swap(arr, i+1, high);
+        
+        // index of the correctly positioned pivot
+        return i+1;
     } 
+    
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
       low  --> Starting index, 
@@ -21,7 +44,13 @@ class QuickSort
     void sort(int arr[], int low, int high) 
     {  
             // Recursively sort elements before 
-            // partition and after partition 
+            // partition and after partition
+        
+        if (low <= high) {
+            int partitionIndex = partition(arr, low, high);
+            sort(arr, 0, partitionIndex - 1);
+            sort(arr, partitionIndex + 1, high);
+        }
     } 
   
     /* A utility function to print array of size n */
