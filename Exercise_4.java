3f48d586374a5c330e8bb388b88a0ffe21ece43b
Sethu Manickam <sethu.sxcs@gmail.com>
2025-06-25 17:36:28 -0700

    Update Exercise_4.java

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,3 +1,6 @@
+// Time Complexity: O(n log n)
+// Space Complexity: O(n log n)
+
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
@@ -5,15 +8,57 @@ class MergeSort
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
-       //Your code here  
+        printArray(arr);
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create temp arrays
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+        
+        
+        int i=0, j=0, k=l;
+        
+       while (i < n1 && j < n2) {
+           if (L[i] <= R[j]) {
+               arr[k++] = L[i++];
+           } else {
+               arr[k++] = R[j++];
+           }
+       }
+        while (i < n1) {
+            arr[k++] = L[i++];
+        }
+        
+        while (j < n2) {
+            arr[k++] = R[j++];
+        }
     } 
-  
+    
+    void swap(int arr[], int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }  
     // Main function that sorts arr[l..r] using 
     // merge() 
     void sort(int arr[], int l, int r) 
     { 
-	//Write your code here
-        //Call mergeSort from here 
+        if (l < r) {
+            int mid = l + (r-l)/2;
+            
+            sort(arr, 0, mid);
+            sort(arr, mid+1, r);
+            
+            merge(arr, l, mid, r);
+        }
     } 
   
     /* A utility function to print array of size n */
